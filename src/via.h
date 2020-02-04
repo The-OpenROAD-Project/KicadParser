c7cf6fc632092cae5e69f88289f6397fdfc59c3b
k2973363@gmail.com <k2973363@gmail.com>
2020-02-04 12:58:37 -0800

    Add supports of MICRO and BLIND_BURIED vias

--- src/via.h
+++ src/via.h
@@ -5,18 +5,26 @@
 #include <vector>
 #include "point.h"
 
+enum class ViaType {
+    THROUGH,
+    MICRO,
+    BLIND_BURIED
+};
+
 class Via {
    public:
     Via(const int id = -1,
         const int netId = -1,
-        const double size = 0)
-        : m_id(id), m_netId(netId), m_size(size) {}
+        const double size = 0.0,
+        const ViaType type = ViaType::THROUGH)
+        : m_id(id), m_netId(netId), m_size(size), m_type(type) {}
 
     int getId() { return m_id; }
     int getNetId() { return m_netId; }
     double getSize() { return m_size; }
     point_2d &getPos() { return m_pos; }
     std::vector<std::string> &getLayers() { return m_layers; }  //layers[0] start layer; layers[1] end layer
+
     void setPosition(const point_2d &_pos) { m_pos = std::move(_pos); }
     void setLayer(const std::vector<std::string> &_layers) { m_layers = std::move(_layers); }
 
@@ -26,6 +34,7 @@ class Via {
     double m_size;  //Drill or Diameter?
     std::vector<std::string> m_layers;
     point_2d m_pos;
+    ViaType m_type;
 };
 
 #endif
